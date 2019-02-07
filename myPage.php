3676151a9206d785c6218414393da2239ff1c398
chathura002 <chathura002@gmail.com>
2019-02-07 14:15:54 +1300

    Updates

---
+++ myPage.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>C-Creations</title>
+<style type="text/css">
+.myPageContainer {
+	float: left;
+	height: 180px;
+	width: 500px;
+}
+.myPageLogo {
+	background-image: url(images/myPageLogo.png);
+	background-repeat: no-repeat;
+	background-position: left top;
+	height: 50px;
+}
+.myPageText {
+	float: left;
+	height: auto;
+	width: 500px;
+	color: #666;
+	font-family: Arial, Helvetica, sans-serif;
+	font-size: 12px;
+}
+</style>
+</head>
+
+<body>
+<div class="myPageContainer">
+  <div class="myPageLogo"></div>
+  <div class="myPageText">
+    <p>We appreciate your interest in our web design services! If you have any questions, please feel free to contact us via  email for more information.</p>
+    <p>chathura002@gmail.com</p>
+    <p>visit http://www.rsautogear.com/ see our other projects.</p>
+</div>
+</div>
+</body>
+</html>
